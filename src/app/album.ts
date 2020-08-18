d740e54170c7e8de651294cd23d52b35731b5fe0
albertmarsnik <albertmarsnik@gmail.com>
2020-08-18 16:40:52 +0200

    task/module-4-using-interfaces-to-describe-data _ All tests passing, noticed some inconsistencies between album.json and the tests they have for the album interface, found a workaround.

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,6 +1,10 @@
+import { SubAlbum } from './sub-album';
 import { Track } from './track';
 
 export interface Album {
+  id: number;
+  artist: string;
+  album: SubAlbum;
   name: string;
   releaseDate: string;
   coverImage: string;
